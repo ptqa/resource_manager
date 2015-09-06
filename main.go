e0fc262da53454ab66020d82147667fa8fe32635
ptQa <ptqa.mail@gmail.com>
2015-09-06 23:24:13 +0200

    proper sorting

--- main.go
+++ main.go
@@ -4,11 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/bradfitz/slice"
-	//"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 	"io/ioutil"
 	"log"
-	//"net/http"
-	//"strconv"
+	"net/http"
 )
 
 type config struct {
@@ -30,13 +29,11 @@ func test() string {
 	return "Tests ok\n"
 }
 
-func (a aresource) sort() {
-	slice.Sort(a, func(i, j int) bool {
-		if a.members[i].state == true && a.members[j].state == false {
-			return true
-		}
-		return false
-	})
+func (a *aresource) Less(i, j int) bool {
+	if a.members[i].state == true && a.members[j].state == false {
+		return true
+	}
+	return false
 }
 
 func main() {
@@ -53,48 +50,43 @@ func main() {
 	fmt.Printf("Loaded config: port %d, limit %d\n", appConfig.Port, appConfig.Limit)
 
 	// Init arr
-	arr := []resource{}
+	arr := aresource{members: []resource{}}
 	for i := 0; i < appConfig.Limit; i++ {
 		r := resource{i, false, "owner1"}
-		arr = append(arr, r)
+		arr.members = append(arr.members, r)
 	}
 
 	//fmt.Println("Allocated arr size:", len(arr))
-	arr[2] = resource{99, true, "wut"}
-	arr[1] = resource{0, true, "wut2"}
-	slice.Sort(arr, func(i, j int) bool {
-		if arr[i].state == true && arr[j].state == false {
-			return true
-		}
-		return false
+	arr.members[2] = resource{99, true, "wut"}
+	arr.members[1] = resource{0, true, "wut2"}
+	slice.Sort(arr.members, arr.Less)
+	fmt.Println("arr: ", arr)
+
+	server := gin.Default()
+	server.GET("/test", func(c *gin.Context) {
+		c.String(http.StatusOK, test())
 	})
-	//fmt.Println("arr: ", arr)
 
 	/*
-		server := gin.Default()
-		server.GET("/test", func(c *gin.Context) {
-			c.String(http.StatusOK, test())
+		server.GET("/data/", func(c *gin.Context) {
+			data := <-input
+			c.String(http.StatusOK,
+			"Id: %d, Name: %s, Data: %s \n",
+			data.id,
+			data.name,
+			data.data)
 		})
 
-			server.GET("/data/", func(c *gin.Context) {
-				data := <-input
-				c.String(http.StatusOK,
-					"Id: %d, Name: %s, Data: %s \n",
-					data.id,
-					data.name,
-					data.data)
-			})
-
-			server.PUT("/data/:id/:name/:data", func(c *gin.Context) {
-				id, err := strconv.Atoi(c.Param("id"))
-				if err != nil {
-					c.String(http.StatusInternalServerError, "Can't convert id to integer\n")
-				}
-				go func() {
-					input <- Data{id: id, name: c.Param("name"), data: c.Param("data")}
-				}()
-				c.String(http.StatusOK, "OK!")
-			})
-		server.Run(":" + strconv.Itoa(appConfig.Port))
+		server.PUT("/data/:id/:name/:data", func(c *gin.Context) {
+			id, err := strconv.Atoi(c.Param("id"))
+			if err != nil {
+				c.String(http.StatusInternalServerError, "Can't convert id to integer\n")
+			}
+			go func() {
+				input <- Data{id: id, name: c.Param("name"), data: c.Param("data")}
+			}()
+			c.String(http.StatusOK, "OK!")
+		})
 	*/
+	server.Run(fmt.Sprintf(":%d", appConfig.Port))
 }
