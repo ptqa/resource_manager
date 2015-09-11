58b31199624074740caaf2bac667eefb610b98d3
ptQa <ptqa.mail@gmail.com>
2015-09-12 00:27:29 +0200

    use hashring

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	//"github.com/bradfitz/slice"
+	"github.com/dgryski/go-jump"
 	"github.com/gin-gonic/gin"
 	"io/ioutil"
 	"log"
@@ -11,8 +12,9 @@ import (
 )
 
 type config struct {
-	Port  int
-	Limit int
+	Port    int
+	Limit   int
+	Workers int
 }
 
 type resource struct {
@@ -57,7 +59,12 @@ func worker(c <-chan message, arr *aresource, freeList chan<- int) {
 
 }
 
-func try_allocate(name string, input []chan message, freeList <-chan int) (int, string) {
+func choose_worker(i int, n int) int {
+	i64 := uint64(i)
+	place := jump.Hash(i64, n)
+	return int(place)
+}
+func try_allocate(name string, input []chan message, freeList <-chan int, workers int) (int, string) {
 	var i, httpStatus int
 	var httpMsg string
 	select {
@@ -65,7 +72,8 @@ func try_allocate(name string, input []chan message, freeList <-chan int) (int, 
 		output := make(chan bool)
 		res := resource{id: i, free: false, owner: name}
 		msg := message{data: res, ch: output}
-		input[i] <- msg
+		place := choose_worker(i, workers)
+		input[place] <- msg
 		result := <-output
 		if result == true {
 			httpStatus = 200
@@ -78,14 +86,15 @@ func try_allocate(name string, input []chan message, freeList <-chan int) (int, 
 	return httpStatus, httpMsg
 }
 
-func try_deallocate(id int, input []chan message, freeList <-chan int) (int, string) {
+func try_deallocate(id int, input []chan message, freeList <-chan int, workers int) (int, string) {
 	id--
 	var httpStatus int
 	var httpMsg string
 	output := make(chan bool)
 	res := resource{id: id, free: true, owner: ""}
 	msg := message{data: res, ch: output}
-	input[id] <- msg
+	place := choose_worker(id, workers)
+	input[place] <- msg
 	result := <-output
 	if result == true {
 		httpStatus = 204
@@ -108,29 +117,34 @@ func main() {
 	if err := json.Unmarshal(configFile, &appConfig); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Loaded config: port %d, limit %d\n", appConfig.Port, appConfig.Limit)
+	if appConfig.Port < 1 || appConfig.Limit < 1 || appConfig.Workers < 2 {
+		panic(fmt.Sprintf("Invalid config\n"))
+	}
+	fmt.Printf("Loaded config: port %d, limit %d, workers %d\n", appConfig.Port, appConfig.Limit, appConfig.Workers)
 
 	// Init arr
 	arr := aresource{members: []resource{}}
+
 	freeList := make(chan int, appConfig.Limit)
 	var input []chan message
+
 	for i := 0; i < appConfig.Limit; i++ {
 		r := resource{i + 1, true, ""}
-		arr.members = append(arr.members, r)
-		ch := make(chan message, 10)
-		input = append(input, ch)
 		freeList <- i
-		go worker(ch, &arr, freeList)
+		arr.members = append(arr.members, r)
 	}
 
-	//slice.Sort(arr.members, arr.Less)
-	//fmt.Println("arr: ", arr)
+	for i := 0; i < appConfig.Workers; i++ {
+		ch := make(chan message, 10)
+		input = append(input, ch)
+		go worker(ch, &arr, freeList)
+	}
 
 	// Starting gin gonic
 	server := gin.Default()
 
 	server.GET("/allocate/:name", func(c *gin.Context) {
-		httpStatus, httpMsg := try_allocate(c.Param("name"), input, freeList)
+		httpStatus, httpMsg := try_allocate(c.Param("name"), input, freeList, appConfig.Workers)
 		c.String(httpStatus, httpMsg)
 	})
 
@@ -142,7 +156,7 @@ func main() {
 			httpMsg = "Not allocated\n"
 			httpStatus = 404
 		} else {
-			httpStatus, httpMsg = try_deallocate(id, input, freeList)
+			httpStatus, httpMsg = try_deallocate(id, input, freeList, appConfig.Workers)
 		}
 		c.String(httpStatus, httpMsg)
 	})
